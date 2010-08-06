cad070655e1833b22da8cd9d6526d1201a98424b
sneek0r <waldemar.smirnow@gmail.com>
2010-08-06 02:03:47 +0200

    record activity created and linked

--- src/org/sport/tracker/Launcher.java
+++ src/org/sport/tracker/Launcher.java
@@ -3,8 +3,11 @@ package org.sport.tracker;
 import org.sport.tracker.utils.ProfilesOnItemSelectedListener;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Launcher extends Activity {
@@ -14,7 +17,29 @@ public class Launcher extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher);
         
+        // no more needed
         Spinner profile_spinner = (Spinner) findViewById(R.id.sp_profile);
         profile_spinner.setOnItemSelectedListener(new ProfilesOnItemSelectedListener());
+        
+        // add record start button listener
+        Button record_start = (Button) findViewById(R.id.bt_start_record);
+        record_start.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				recordStart();
+			}
+		});
+    }
+    
+    public void recordStart() {
+    	// get selected profile
+    	Spinner profile_spinner = (Spinner) findViewById(R.id.sp_profile);
+        final String selected_profile = (String) profile_spinner.getSelectedItem();
+        
+        // start record Activity
+    	Intent record_intent = new Intent(Launcher.this, RecordUI.class);
+    	record_intent.putExtra("profile", selected_profile);
+    	startActivity(record_intent);
     }
 }
