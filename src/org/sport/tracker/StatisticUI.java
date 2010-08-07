38859def1ea93bf178660aa4ef9c27401f3e0602
sneek0r <waldemar.smirnow@gmail.com>
2010-08-07 16:40:42 +0200

    statistic UI added
    Signed-off-by: sneek0r <waldemar.smirnow@gmail.com>

---
+++ src/org/sport/tracker/StatisticUI.java
@@ -0,0 +1,22 @@
+package org.sport.tracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class StatisticUI extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistic);
+        
+        // set profile
+        Bundle extras = getIntent().getExtras();
+        if( extras.containsKey("profile") ) {
+        	TextView profile_tv = (TextView) findViewById(R.id.tv_profile);
+        	profile_tv.setText(extras.getCharSequence("profile"));
+        	profile_tv.postInvalidate();
+        }
+    }
+}
