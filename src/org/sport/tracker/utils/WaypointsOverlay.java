332b8a6a4e4b6e09994ae33501f084f3da5c4073
sneek0r <waldemar.smirnow@gmail.com>
2010-08-26 16:50:39 +0200

    map show waypoints

--- src/org/sport/tracker/utils/WaypointsOverlay.java
+++ src/org/sport/tracker/utils/WaypointsOverlay.java
@@ -2,9 +2,16 @@ package org.sport.tracker.utils;
 
 import java.util.List;
 
+import org.sport.tracker.R;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
 public class WaypointsOverlay extends Overlay {
@@ -17,4 +24,22 @@ public class WaypointsOverlay extends Overlay {
 		this.context = context;
 		this.waypoints = waypoints;
 	}
+	
+	@Override
+    public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
+		super.draw(canvas, mapView, shadow); 
+		
+		
+		for (GeoPoint waypoint : waypoints) {
+			Point point = new Point();
+			mapView.getProjection().toPixels(waypoint, point);
+			Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), R.drawable.punkt);
+			canvas.drawBitmap(bmp, point.x, point.y - 50, null);
+			
+		}
+		
+		if (waypoints.size() > 0) mapView.getController().animateTo(waypoints.get(0));
+		return true;
+    }
+
 }
